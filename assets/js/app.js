0b6799dfcf96c2b7a769bfe5c243156158dc212d
Nahid Hasan <NahidPundra.CSE@gmail.com>
2022-04-12 17:28:18 +0600

    Random math generate

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,82 @@ const score_field = document.querySelector("#score");
 let submitted_result = 0;
 let number1 = 0,
   number2 = 0,
-  operator = "+",
-  result = 0;
-let score = 0,
+  operator = "",
+  result = 0,
+  score = 0,
   live = 5;
+expression = "";
 
-function generate() {}
+function generateOperator() {
+  let random_operator = Math.floor(Math.random() * 8);
+  switch (random_operator) {
+    case 0:
+    case 1:
+    case 2:
+      operator = "+";
+      break;
+    case 3:
+    case 4:
+      operator = "-";
+      break;
+    case 5:
+    case 6:
+      operator = "*";
+      break;
+    case 7:
+      operator = "/";
+      break;
+  }
+}
+
+function generateNumber(score) {
+  let level = Math.floor(Math.log(score / 50) / Math.log(3));
+  if (!level) level = 0;
+  else if (level > 4) level = 4;
+  const limits = [21, 101, 501, 1001, 5001, 6, 11, 21, 31, 51];
+  let limit = 0;
+  generateOperator();
+  if (operator == "+" || operator == "-") limit = limits[level];
+  else limit = limits[5 + level];
+  number1 = Math.floor(Math.random() * limit);
+  number2 = Math.floor(Math.random() * limit);
+  if (number1 < number2) {
+    number1 = number1 + number2;
+    number2 = number1 - number2;
+    number1 = number1 - number2;
+  }
+}
+
+function getMath() {
+  generateNumber();
+  switch (operator) {
+    case "+":
+      expression = number1 + " + " + number2;
+      result = number1 + number2;
+      break;
+    case "-":
+      expression = number1 + " - " + number2;
+      result = number1 - number2;
+      break;
+    case "*":
+      expression = number1 + " * " + number2;
+      result = number1 * number2;
+      break;
+    case "/":
+      if (number2 == 0) number2++;
+      result = number1 * number2;
+      result = result + number1;
+      number1 = result - number1;
+      result = result - number1;
+      expression = number1 + " / " + number2;
+      result = number1 / number2;
+      break;
+  }
+  console.log(expression, " = ", result);
+}
 
 function onSubmit() {
+  
   score_field.innerHTML = score;
   submitted_result = 0;
   my_result.innerHTML = submitted_result;
